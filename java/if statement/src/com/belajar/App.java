f517ce0fdfdf2afbc6fc26d783270db56733884f
haris2303 <hrsccf102@gmail.com>
2022-11-26 15:20:48 +0900

    initial commit

--- java/if statement/src/com/belajar/App.java
+++ java/if statement/src/com/belajar/App.java
@@ -1,7 +1,5 @@
 package com.belajar;
 
-
-
 public class App {
 
   public static void main(String[] args) {
@@ -9,7 +7,41 @@ public class App {
     // if statement
     // => sebuah perkondisian yang dimana if hanya mengandung nilai true or false
 
+    // String satu = "satu";
 
+    // if(satu == "satu") {
+    //   System.out.println("ini Bisa!!");
+    // }
+
+    // String nama1 = "Gery";
+    // String nama2 = "Gery";
+
+    // if (nama1 == nama2) {
+    //   System.out.println("Ini Bisa");
+    // }
+
+    /* 
+     * jika nilai yang didapatkan > 90 maka tampilkan nilai A
+     * jika nilai yan didapatkan > 80 maka tampilkan nilai B
+     * jika nilai yang didapatkan > 70 maka tampilkan nilai C
+     * selain itu tampilkan nilai D
+     */
+
+    //  char char1 = 'A';
+
+    //  if(char1 == 'A') {
+    //   System.out.println("Char ini sama dengan A");
+    //  }
+    
+    int nilai = 160;
+
+    if ( nilai < 50 ) {
+      System.out.println("Nilai " + nilai + " terlalu kecil");
+    } else if ( nilai > 50 && nilai < 100 ) {
+      System.out.println("Nilai " + nilai + " terlalu besar");
+    } else {
+      System.out.println("Nilai Kocak");
+    }
 
   }
 
